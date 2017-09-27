205630997a1eda7ff1d22a75165e183022beea75
Stephen Collins <stephenc211@gmail.com>
2017-09-26 23:02:28 -0500

    added BinaryTree private search and insert

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -14,7 +14,7 @@ class BinaryTree {
   private:
     node *root;
     void insert(int data, node *leaf);
-    void *search(int data, node *leaf);
+    node *search(int data, node *leaf);
     void destroy_tree(node *leaf);
 
   public: 
@@ -27,7 +27,7 @@ class BinaryTree {
 };
 
 BinaryTree::BinaryTree() {
-  root=NULL;
+  root = NULL;
 }
 
 BinaryTree::~BinaryTree() {
@@ -35,7 +35,7 @@ BinaryTree::~BinaryTree() {
 }
 
 void BinaryTree::destroy_tree(node *leaf) {
-  if (leaf!=NULL) {
+  if (leaf != NULL) {
     destroy_tree(leaf->left);
     destroy_tree(leaf->right);
     delete leaf;
@@ -44,4 +44,38 @@ void BinaryTree::destroy_tree(node *leaf) {
 
 void BinaryTree::insert(int data, node *leaf) {
   // TODO: continue on private insert binary tree node method
+  if (data < leaf->data) {
+    if (leaf->left != NULL) {
+      insert(data, leaf->left);
+    } else {
+      leaf->left = new node;
+      leaf->left->data = data;
+      leaf->left->left = NULL;
+      leaf->left->right = NULL;
+    }
+  } else if (data >= leaf->data) {
+    if (leaf->right != NULL) {
+      insert(data, leaf->right);
+    } else {
+      leaf->right = new node;
+      leaf->right->data = data;
+      leaf->right->left = NULL;
+      leaf->right->right = NULL;
+    }
+  }
+}
+
+node* BinaryTree::search(int data, node *leaf) {
+  if (leaf != NULL) {
+    if (data == leaf->data ) {
+      return leaf;
+    }
+    if (data < leaf->data) {
+      return search(data, leaf->left);
+    } else {
+      return search(data, leaf->right);
+    }
+  } else {
+    return NULL;
+  }
 }
